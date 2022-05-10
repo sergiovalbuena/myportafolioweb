0e4d5e1ec634dc7999a7a838042931c77ea7597f
srji <iosergio.valbuena@gmail.com>
2022-05-10 12:08:32 -0700

    menu added, menu passed as data, css with props as []length menu. or props in case the title of the data is empty so the padding will be different

---
+++ src/components/buttons/MenuButton.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+
+export default function MenuButton(props) {
+    const {item} = props
+  return (
+    <Link to={item.link}>
+      <MenuItem title={item.title}>
+        <img src={item.icon} alt={item.title} />
+        {item.title}
+      </MenuItem>
+    </Link>
+  )
+}
+
+const MenuItem = styled.div`
+    color: rgba(255,255,255,.7);
+    display: grid ;
+    grid-template-columns: 24px auto ;
+    //gap: 10px;
+    gap: ${props => props.title ? '10px' : '0' };
+    align-items:center ;
+    padding: 10px;
+    transition: .7s ease-out ;
+
+    :hover{
+        background:rgba(255,255,255,.1) ;
+        box-shadow:0 10px 20px rgba(0,0,0,.1),
+        inset 0 0 0 .5px rgba(255,255,255,.2) ;
+        border-radius: 15px ;
+    }
+`
---
+++ src/data/menuData.js
@@ -0,0 +1,27 @@
+export const menuData = [
+    {
+      title: "About",
+      icon: "/images/icons/courses.svg",
+      link: "/about",
+    },
+    {
+      title: "Projects",
+      icon: "/images/icons/courses.svg",
+      link: "/projects",
+    },
+    {
+      title: "Tips",
+      icon: "/images/icons/courses.svg",
+      link: "/tips",
+    },
+    {
+      title: "",
+      icon: "/images/icons/account.svg",
+      link: "/account",
+    },
+    {
+      title: "",
+      icon: "/images/icons/account.svg",
+      link: "/account",
+    },
+  ]
