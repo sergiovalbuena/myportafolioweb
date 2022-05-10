a530ad68b53a18c5a0c6714b897f3ebcc2b1fc67
srji <iosergio.valbuena@gmail.com>
2022-05-10 03:08:24 -0700

    mockups Hero added, animation hover too

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import ContactButton from "../buttons/ContactButton"
+import MockupAnimation from "../animations/MockupAnimation"
 
 //const HeroSection = () => {return(<div></div>)}
 function HeroSection() {
@@ -13,9 +14,13 @@ function HeroSection() {
         <TextWrapper>
           <Title>Hi,</Title>
                   <Description>Happy Hacking</Description>
-                  <ContactButton/>
+          <ContactButton 
+            //title='Contactame ahora'
+            //subtitle='Codificando el mundo'
+                  />
           <Link to="/page-2">Go To Page 2</Link>
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -25,12 +30,18 @@ export default HeroSection
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden ;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
+  height: 800px;
   margin: 0 auto;
   padding: 200px 30px;
+  display:grid ;
+  grid-template-columns:360px auto ;
+  
+
 `
 const TextWrapper = styled.div`
   max-width: 360px;
